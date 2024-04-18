f09604562538567048555c25af1ab0e6487438b4
MRxwr <nasserhatab@gmail.com>
2024-04-19 02:35:53 +0300

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -4,7 +4,8 @@ if ( isset($_POST["username"]) && !empty($_POST["username"] )){
 	$_POST = str_replace($check,"",$_POST);
 	require_once('includes/config.php');
 	require_once('includes/functions.php');
-	if ( $user = selectDBUpdated("user","`username` LIKE '".$_POST['username']."' AND `password` LIKE '".sha1($_POST['password'])."' AND `status` LIKE '0'") ){
+	if ( $user = selectDB("user","`username` LIKE '".$_POST['username']."' AND `password` LIKE '".sha1($_POST['password'])."' AND `status` LIKE '0'") ){
+		die();
 		setcookie('ezyoCreate', md5(time().$_POST['username']), time() + (3600*24*30) , '/');
 		$dataUpdate = array(
 			"cookie" => md5(time().$_POST['username']),
